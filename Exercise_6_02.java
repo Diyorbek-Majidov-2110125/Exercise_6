9a5353ef2aef6428d95e3e60b9f531b4447389ad
Diyorbek-Majidov-2110125 <115059245+Diyorbek-Majidov-2110125@users.noreply.github.com>
2022-11-03 17:54:49 +0500

    Add files via upload

---
+++ Exercise_6_02.java
@@ -0,0 +1,19 @@
+package project_1.All;
+
+import java.util.Scanner;
+
+public class Exercise_6_02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: "); long number = input.nextLong();
+        int result = sumDigits(number);
+        System.out.println("Sum of digits in the number: " + result);
+    }
+    public static int sumDigits(long n){
+        int sum = 0;
+        for(int i = 0; i < 18; i++){
+            sum += (n / Math.pow(10, i)) % 10;
+        }
+        return sum;
+    }
+}
